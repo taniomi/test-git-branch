67fede112224cf0373fa496a456630c3a2a780df
taniomi <131834608+taniomi@users.noreply.github.com>
2023-12-19 12:57:31 -0300

    [ADD] Function gather_data()

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+def gather_data():
+    n1 = int(input())
+    n2 = int(input())
+
+    return n1, n2
+
 def main():
-    print(2*2)
+    n1, n2 = gather_data()
+    print(n1*n2)
+
     return None
 
 if __name__ == "__main__":
