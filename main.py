4da20662a8ca83b0ec8570aa59b4190fb139d23c
taniomi <131834608+taniomi@users.noreply.github.com>
2023-12-20 10:46:48 -0300

    [ADD] Fuction print_msg()

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def gather_data():
 
     return n1, n2
 
+def print_msg(n1, n2):
+    print(f'{n1} + {n2} = {n1+n2}')
+    return None
+
 def main():
     n1, n2 = gather_data()
-    print(n1**n2)
 
+    print_msg(n1, n2)
     return None
 
 if __name__ == "__main__":
