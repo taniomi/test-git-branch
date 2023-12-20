04c6d3aa9aec6ca8761ee76ead069555909ff799
taniomi <131834608+taniomi@users.noreply.github.com>
2023-12-20 10:31:39 -0300

    [ADD] Function get_op()

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+def get_op():
+    op = input()
+    return op
+
 def gather_data():
     n1 = input()
     n2 = input() 
-    op = input()
-
     return n1, n2, op
 
 def main():
     n1, n2, op = gather_data()
     print(eval(n1+op+n2))
-
     return None
 
 if __name__ == "__main__":
